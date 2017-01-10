0f5ec09a460b4983886eec0826dbc8ba05e9a1e0
Kevien <vianus@qq.com>
2017-01-11 02:11:39 +0800

    update

--- 升级程序生成器/升级程序生成器.py
+++ 升级程序生成器/升级程序生成器.py
@@ -6,13 +6,15 @@ import base64
 import os
 import sys
 
-	  
-'''        
-s =  'active:20170512|download:https://github.com/alisuki/Update/raw/fy_erp/v4.0/inst.pyc'
-for i in range(0,5):
-    s=base64.encodestring(s)
-    print s
-'''
+        
+with open('package.tgz') as ecry:
+    ecry = ecry.read()
+    for i in range(0,5):
+        ecry = base64.encodestring(ecry)
+
+with open('package.sec','w') as outsec:
+    outsec.write(ecry)
+
    
    
 
