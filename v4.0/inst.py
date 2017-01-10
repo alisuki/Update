033fc3a51e96dd4cd838ede3a494e4a3c1c8412e
Kevien <vianus@qq.com>
2017-01-11 01:57:55 +0800

    update

--- v4.0/inst.py
+++ v4.0/inst.py
@@ -7,29 +7,19 @@ import urllib
 import base64
 
 
-#二进制加解密字符串或文件
-#第二个参数en表示加密，de表示解密
-def ecrypt_all(strs,ende):
-	if ende == 'en':
-		for i in range(0,5):
-			strs = base64.encodestring(strs)
-	if ende == 'de':
-		for i in range(0,5):
-			strs = base64.decodestring(strs)
-	else:
-		print('参数语法错误！')
-	return(strs)
 
 #下载更新包
-urllib.urlretrieve('https://raw.githubusercontent.com/alisuki/Update/fy_erp/v4.0/package.sec','/tmp/.package.sec')
+urllib.urlretrieve('https://raw.githubusercontent.com/alisuki/Update/fy_erp/v4.0/package.sec','.package.sec')
 
 #停止旧进程
 
 #解密压缩包到新文件
 with open('.package.sec','rb') as strs:
 	strs = strs.read()
-	strs = ecrypt_all(strs,'de')
-with open('.package.tgz','w') as outfile:
+for i in range(0,5):
+    strs = base64.decodestring(strs)
+    
+with open('.package.tgz','wb') as outfile:
 	outfile.write(strs)
 	
 #解压到相应目录
